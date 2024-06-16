beb26b5f5529a7af0b9415fd2b4c1a16a0e11245
Kevin Carrasco <kevin.carrasco@ecofor.cl>
2024-06-16 18:58:35 -0400

    avance express get post

--- 2.- API/3.express.js
+++ 2.- API/3.express.js
@@ -7,7 +7,19 @@ app.disable('x-powered-by')
 
 app.use((req, res, next) => {
   console.log('mi primer middleware')
-  next()
+  if (req.method !== 'POST') return next();
+  if (req.headers['content-type'] !== 'application/json') return next();
+  let body = ''
+  // escuchar evento data
+  req.on('data', chunk => {
+    body += chunk.toString()
+  })
+  req.on('end', () => {
+    const data = JSON.parse(body);
+    data.timestamp = Date.now();
+    req.body = data;
+    next();
+  })
 })
 
 app.get('/pokemon/ditto', (req, res) => {
@@ -15,15 +27,7 @@ app.get('/pokemon/ditto', (req, res) => {
 })
 
 app.post('/pokemon', (req, res) => {
-  let body = ''
-  // escuchar evento data
-  req.on('data', chunk => {
-    body += chunk.toString()
-  })
-  req.on('end', () => {
-    const data = JSON.parse(body)
-    res.status(201).json(data)
-  })
+    res.status(201).json(req.body)
 })
 
 app.use((req, res) => {
