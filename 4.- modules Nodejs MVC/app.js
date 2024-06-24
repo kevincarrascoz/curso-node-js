9927d56f273b4585ee09c4a509ba5c2a1bada737
Kevin Carrasco <kevin.carrasco@ecofor.cl>
2024-06-23 22:49:07 -0400

    mvc ems modules node

---
+++ 4.- modules Nodejs MVC/app.js
@@ -0,0 +1,16 @@
+import express, { json } from 'express'
+import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
+//import fs from 'node:fs'
+//const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
+
+const app = express()
+app.use(json())
+app.use(corsMiddleware())
+app.use('/movies', moviesRouter)
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
